d3e561d262f6d492f69e07b68d767257da53e1b3
Babakhan <narynbekuulu@gmail.com>
2024-10-06 21:12:11 +0600

    method save

--- src/main/java/org/example/java15/dao/daoImpl/MenultemDaoImpl.java
+++ src/main/java/org/example/java15/dao/daoImpl/MenultemDaoImpl.java
@@ -1,4 +1,20 @@
 package org.example.java15.dao.daoImpl;
 
-public class MenultemDaoImpl {
+import org.example.java15.dao.MenultemDao;
+import org.example.java15.db.Database;
+import org.example.java15.model.Menultem;
+
+public class MenultemDaoImpl implements MenultemDao {
+    private final Database database;
+
+    public MenultemDaoImpl(Database database) {
+        this.database = database;
+    }
+
+
+    @Override
+    public String save(Menultem menuItem) {
+        database.menultems.add(menuItem);
+        return "Successfully saved";
+    }
 }
